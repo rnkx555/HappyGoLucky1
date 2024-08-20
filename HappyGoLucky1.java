c76f7b116f5bc1f6e9931c7799e5f00f17f6daf9
rnkx555 <p22013872@student.newinti.edu.my>
2024-08-20 22:20:28 +0800

    Add files via upload

---
+++ HappyGoLucky1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.happygolucky1;
+
+/**
+ *
+ * @author rache
+ */
+public class HappyGoLucky1 {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the GitHub");
+    }
+}
